922afc8cebbf609ae7ba1e27f70c1cc76b76854d
M01tyan <kanta01m.tyan@gmail.com>
2020-05-07 14:37:34 +0900

    SendGrid再テスト

--- src/Component/SendMail.java
+++ src/Component/SendMail.java
@@ -14,19 +14,39 @@ public class SendMail {
 
 	public String send(String email) throws IOException {
 		String code = createAuthCode();
-		Email from = new Email("sakura.symoney@gmail.com");
-	    String mailSubject = "Authentication Email";
-	    Email to = new Email(email);
-	    Content mailContent = new Content("text/plain",
-	    		"認証コードをお送りします。\n\n"
-	    		+ code + "\n\n"
-	    		+ "アプリの方で認証コードを入力し、\n"
-	    		+ "アカウントを有効にしてください。"
-	    		+ "なお30分以内に認証しない場合は失効します");
-	    Mail mail = new Mail(from, mailSubject, to, mailContent);
-	    String sendGridAPI = System.getenv("SENDGRID_API_KEY");
-	    System.out.println(sendGridAPI);
-	    SendGrid sg = new SendGrid(sendGridAPI);
+//		Email from = new Email("test@example.com");
+//	    String mailSubject = "Authentication Email";
+//	    Email to = new Email("test@example.com");
+//	    Content mailContent = new Content("text/plain",
+//	    		"認証コードをお送りします。\n\n"
+//	    		+ code + "\n\n"
+//	    		+ "アプリの方で認証コードを入力し、\n"
+//	    		+ "アカウントを有効にしてください。"
+//	    		+ "なお30分以内に認証しない場合は失効します");
+//	    Mail mail = new Mail(from, mailSubject, to, mailContent);
+//	    String sendGridAPI = System.getenv("SENDGRID_API_KEY");
+//	    System.out.println(sendGridAPI);
+//	    SendGrid sg = new SendGrid(sendGridAPI);
+//	    Request request = new Request();
+//	    try {
+//	      request.setMethod(Method.POST);
+//	      request.setEndpoint("mail/send");
+//	      request.setBody(mail.build());
+//	      Response response = sg.api(request);
+//	      System.out.println(response.getStatusCode());
+//	      System.out.println(response.getBody());
+//	      System.out.println(response.getHeaders());
+//	    } catch (IOException ex) {
+//	      throw ex;
+//	    }
+		Email from = new Email("test@example.com");
+	    String subject = "Sending with SendGrid is Fun";
+	    Email to = new Email("test@example.com");
+	    Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+	    Mail mail = new Mail(from, subject, to, content);
+
+	    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+	    System.out.println(System.getenv("SENDGRID_API_KEY"));
 	    Request request = new Request();
 	    try {
 	      request.setMethod(Method.POST);
