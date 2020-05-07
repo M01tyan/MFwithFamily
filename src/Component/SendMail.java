e2343215a95e6337e107a1a284c915182bbeee9f
M01tyan <kanta01m.tyan@gmail.com>
2020-05-07 14:11:53 +0900

    SendGrid再テスト

--- src/Component/SendMail.java
+++ src/Component/SendMail.java
@@ -2,35 +2,43 @@ package Component;
 
 import java.io.IOException;
 
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+
 public class SendMail {
 
 	public String send(String email) throws IOException {
 		String code = createAuthCode();
-//		Email from = new Email("sakura.symoney@gmail.com");
-//	    String mailSubject = "Authentication Email";
-//	    Email to = new Email(email);
-//	    Content mailContent = new Content("text/plain",
-//	    		"認証コードをお送りします。\n\n"
-//	    		+ code + "\n\n"
-//	    		+ "アプリの方で認証コードを入力し、\n"
-//	    		+ "アカウントを有効にしてください。"
-//	    		+ "なお30分以内に認証しない場合は失効します");
-//	    Mail mail = new Mail(from, mailSubject, to, mailContent);
-//	    String sendGridAPI = System.getenv("SENDGRID_API_KEY");
-//
-//	    SendGrid sg = new SendGrid(sendGridAPI);
-//	    Request request = new Request();
-//	    try {
-//	      request.setMethod(Method.POST);
-//	      request.setEndpoint("mail/send");
-//	      request.setBody(mail.build());
-//	      Response response = sg.api(request);
-//	      System.out.println(response.getStatusCode());
-//	      System.out.println(response.getBody());
-//	      System.out.println(response.getHeaders());
-//	    } catch (IOException ex) {
-//	      throw ex;
-//	    }
+		Email from = new Email("sakura.symoney@gmail.com");
+	    String mailSubject = "Authentication Email";
+	    Email to = new Email(email);
+	    Content mailContent = new Content("text/plain",
+	    		"認証コードをお送りします。\n\n"
+	    		+ code + "\n\n"
+	    		+ "アプリの方で認証コードを入力し、\n"
+	    		+ "アカウントを有効にしてください。"
+	    		+ "なお30分以内に認証しない場合は失効します");
+	    Mail mail = new Mail(from, mailSubject, to, mailContent);
+	    String sendGridAPI = System.getenv("SENDGRID_API_KEY");
+	    System.out.println(sendGridAPI);
+	    SendGrid sg = new SendGrid(sendGridAPI);
+	    Request request = new Request();
+	    try {
+	      request.setMethod(Method.POST);
+	      request.setEndpoint("mail/send");
+	      request.setBody(mail.build());
+	      Response response = sg.api(request);
+	      System.out.println(response.getStatusCode());
+	      System.out.println(response.getBody());
+	      System.out.println(response.getHeaders());
+	    } catch (IOException ex) {
+	      throw ex;
+	    }
 	    return code;
 	}
 
