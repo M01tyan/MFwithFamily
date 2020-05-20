ef132e4ff6cbf0c23f1150bfc60af980fb6513eb
M01tyan <kanta01m.tyan@gmail.com>
2020-05-20 16:17:42 +0900

    金額にカンマをつけた

--- src/Controller/SignUp.java
+++ src/Controller/SignUp.java
@@ -37,6 +37,7 @@ public class SignUp extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
 		//Formから入力された文字列を受け取る
 		String email = (String)request.getParameter("email");
 		String password = (String)request.getParameter("password");
@@ -139,7 +140,7 @@ public class SignUp extends HttpServlet {
 	 * @throws ClassNotFoundException jdbcドライバが存在しない場合
 	 */
 	private User createUser(String email, String password, String name) throws SQLException, ClassNotFoundException {
-		System.out.println(email + " : " + password);
+		System.out.println(email + " : " + password + " " + name);
 		//MySQLへの接続
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		String url = "jdbc:" + System.getenv("HEROKU_DB_URL") + "?reconnect=true&verifyServerCertificate=false&useSSL=true&characterEncoding=utf8";
